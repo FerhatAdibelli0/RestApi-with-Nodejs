0e5e1fb89f5d430f37d865fdc597d672d1833556
Maxpayne <ferhatadibelli9@gmail.com>
2022-04-27 00:28:35 +0300

    fixed error

--- App.js
+++ App.js
@@ -8,11 +8,14 @@ const app = express();
 app.use(bodyParser.json());
 
 // app.use(cors());  //Using Cors Library
-// Another way to solve Cors 
+// Another way to solve Cors
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "GET,POST,DELETE,PUT,PATCH");
-  res.setHeader("Access-Control-Allow_Header", "Content-Type,Authorization");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "OPTİON,GET,POST,DELETE,PUT,PATCH"
+  );
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type,Authorization");
   next();
 });
 
