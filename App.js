5c474db857e5cea4b155140adb3043f8b1630615
Maxpayne <ferhatadibelli9@gmail.com>
2022-05-03 18:40:44 +0300

    socket.io setup

--- App.js
+++ App.js
@@ -68,6 +68,10 @@ app.use((error, req, res, next) => {
 mongoose
   .connect(MONGO_URI)
   .then((result) => {
-    app.listen(8000);
+    const server = app.listen(8000);
+    const io = require("socket.io")(server);
+    io.on("connection", (socket) => {
+      console.log("Client connected");
+    });
   })
   .catch((err) => console.log(err));
