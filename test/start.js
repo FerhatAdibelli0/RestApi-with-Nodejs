fbea3ca1ca1836ea6e03e9da67811fc29eab2629
Maxpayne <ferhatadibelli9@gmail.com>
2022-05-12 19:32:19 +0300

    mocha and chai added

---
+++ test/start.js
@@ -0,0 +1,13 @@
+const expect = require("chai").expect;
+
+it("should give correct result", () => {
+  const a = 5;
+  const b = 6;
+  expect(a + b).to.equal(11);
+});
+
+it("shouldnt give correct result", () => {
+  const a = 5;
+  const b = 6;
+  expect(a + b).not.to.equal(14);
+});
