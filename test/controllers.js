d58b581ec38e324952773e78d0e046665c2de8fa
Maxpayne <ferhatadibelli9@gmail.com>
2022-05-13 22:43:12 +0300

    added gitStatus test

--- test/controllers.js
+++ test/controllers.js
@@ -24,10 +24,13 @@ describe("authController-login", function () {
       .catch((err) => {
         done(err);
       });
+    //   .catch((err) => {
+    //     done(err);
+    //   });
     User.findOne.restore();
   });
 
-  it("should send response with user status for existing user", function () {
+  it("should send response with user status for existing user", function (done) {
     mongoose
       .connect(
         "mongodb+srv://maxpayne35:qGBr7naSXYmEYnw@cluster0.sp51h.mongodb.net/test-messages?retryWrites=true&w=majority"
@@ -39,6 +42,29 @@ describe("authController-login", function () {
           name: "ferhat",
           posts: [],
         });
+        return user.save();
+      })
+      .then((result) => {
+        const req = { userId: "627ea8bf4503101258ea5d50" };
+        const res = {
+          userStatus: null,
+          statusCode: 500,
+          status: function (code) {
+            this.statusCode = code;
+            return this;
+          },
+          json: function (data) {
+            this.userStatus = data.status;
+          },
+        };
+
+        authController
+          .getStatus(req, res, () => {})
+          .then((data) => {
+            expect(res.statusCode).to.be.equal(200);
+            expect(res.userStatus).to.be.equal("I am a user");
+            done();
+          });
       })
       .catch((err) => console.log(err));
   });
